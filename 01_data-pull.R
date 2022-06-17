b65c1cbddd27411df71a5301bffac40f41f738c1
simscr <simscr90@gmail.com>
2022-06-16 19:36:21 -0500

    connect to drive/sheets and pull random workout

--- 01_data-pull.R
+++ 01_data-pull.R
@@ -2,3 +2,27 @@
 # Script to pull in data --------------------------------------------------
 
 pacman::p_load(tidyverse, googlesheets4, googledrive)
+
+# Authorize access to Google Drive for googledrive and googlesheets4
+googledrive::drive_auth()
+gs4_auth(token = drive_token())
+
+# Find the sheet named "workouts"
+drive_find("workouts")
+
+# Get drive information about "workouts"
+wo <- drive_get("workouts")
+
+# Get Sheets location for "workouts"
+gs4_get(wo)
+
+# Load "workouts" sheet from "workouts"
+workouts <- read_sheet(wo, sheet = "workouts")
+
+# Pull Random Workout
+
+n_wo <- length(unique(workouts$workout_number))
+
+workouts %>% 
+  filter(workout_number == sample(1:n_wo, 1)) %>% 
+  view()
