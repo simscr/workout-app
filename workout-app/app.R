8aa27d6a75a59f842ad5aba7686093fc43e3bd72
simscr <simscr90@gmail.com>
2022-09-24 02:56:24 -0500

    keeping updated

--- workout-app/app.R
+++ workout-app/app.R
@@ -10,10 +10,15 @@ googlesheets4::gs4_auth(token = googledrive::drive_token())
 
 sheet_id <- googledrive::drive_get("workouts")$id
 
-workouts <- googlesheets4::read_sheet(ss = sheet_id,
-                       sheet = "workouts") 
+workouts <- googlesheets4::read_sheet(
+  ss = sheet_id,
+  sheet = "workouts"
+)
 
 n_wo <- length(unique(workouts$workout_number))
+
+wo_name <- "Death"
+
 
 # functions ---------------------------------------------------------------
 
@@ -57,22 +62,28 @@ specific_wo <- function(term) {
     gt::cols_align(columns = 3, align = "center")
 }
 
-# Define UI for application that draws a histogram
+
+# ui ----------------------------------------------------------------------
+
+# Define UI for application that draws a workout
 ui <- fluidPage(
 
   # Application title
   titlePanel("Workout Selector"),
 
-  # Sidebar with a slider input for number of bins
+  # Sidebar with a random workout input and a select input
   sidebarLayout(
     sidebarPanel(
-      "Selection Options\n",
+      "Selection Options",
+      hr(), 
       actionButton("random_button", label = "Random Workout"),
-      # numericInput(inputId = "rand_no", label = "Random Number", value = sample(1:n_wo, 1)),
-      textInput("name", label = "Search by Name")
+      hr(),
+      selectInput("name", label = "Search by Name", choices = unique(workouts %>%
+        filter(!is.na(name)) %>%
+        pluck("name")))
     ),
 
-    # Show a plot of the generated distribution
+    # Show a plot of the generated workout
     mainPanel(
       "Here's your workout!",
       gt_output("workoutTable")
@@ -80,32 +91,59 @@ ui <- fluidPage(
   )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw a workout
 server <- function(input, output) {
+  
   
   random_number <- eventReactive(input$random_button, {
     sample(1:n_wo, 1)
   })
   
+  workout_name <- eventReactive(input$name, valueExpr = "Death")
+
+  
+  # workout_choice <- eventReactive(input$random_button, {sample(1:n_wo)})
+  
   output$workoutTable <- render_gt({
-    workouts %>%
-      mutate(notes = snakecase::to_sentence_case(notes)) %>%
-      mutate(
-        # across(where(is.list), ~ na_if(., is.null)),
-        across(where(is.list), as.character)
-      ) %>%
-      filter(workout_number == random_number()) %>%
-      mutate(across(where(is.character), ~ na_if(., "NULL"))) %>%
-      janitor::remove_empty("cols") %>%
-      select(-workout_number) %>%
-      rename_with(snakecase::to_title_case) %>%
-      gt::gt(.) %>%
-      gt::sub_missing(everything(),
-                      missing_text = "---"
-      ) %>%
-      gtExtras::gt_theme_538()
+      workouts %>%
+        mutate(notes = snakecase::to_sentence_case(notes)) %>%
+        mutate(
+          # across(where(is.list), ~ na_if(., is.null)),
+          across(where(is.list), as.character)
+        ) %>%
+        filter(workout_number == random_number()) %>%
+        mutate(across(where(is.character), ~ na_if(., "NULL"))) %>%
+        janitor::remove_empty("cols") %>%
+        select(-workout_number) %>%
+        rename_with(snakecase::to_title_case) %>%
+        gt::gt(.) %>%
+        gt::sub_missing(everything(),
+          missing_text = "---"
+        ) %>%
+        gtExtras::gt_theme_538()
+  
+    # workouts %>%
+    #   mutate(notes = snakecase::to_sentence_case(notes)) %>%
+    #   mutate(
+    #     # across(where(is.list), ~ na_if(., is.null)),
+    #     across(where(is.list), as.character)
+    #   ) %>%
+    #   filter(workout_name == workout_name()) %>%
+    #   mutate(across(where(is.character), ~ na_if(., "NULL"))) %>%
+    #   janitor::remove_empty("cols") %>%
+    #   select(-workout_number) %>%
+    #   rename_with(snakecase::to_title_case) %>%
+    #   gt::gt(.) %>%
+    #   gt::sub_missing(everything(),
+    #                   missing_text = "---"
+    #   ) %>%
+    #   gtExtras::gt_theme_538()
+    
   })
+  
+    
 }
+
 
 # Run the application
 shinyApp(ui = ui, server = server)
